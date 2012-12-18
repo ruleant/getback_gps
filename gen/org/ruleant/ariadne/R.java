5f9eeb8d71f78514536b3799ecdf4dbd57faddd5
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2012-12-18 23:30:35 +0100

    add descriptive message to first screen/Action

--- gen/org/ruleant/ariadne/R.java
+++ gen/org/ruleant/ariadne/R.java
@@ -14,7 +14,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int menu_settings=0x7f070002;
+        public static final int textView1=0x7f070000;
+        public static final int textView2=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -24,8 +26,9 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int first_message=0x7f040002;
         public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int menu_settings=0x7f040003;
     }
     public static final class style {
         /** 
