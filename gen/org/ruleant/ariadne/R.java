dc0958f56ed29cdbc6237426ca97be3b2c3a86df
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2012-12-20 21:34:11 +0100

    add Location Provider and Location Fields to Screen; add code to get and update Location Provider

--- gen/org/ruleant/ariadne/R.java
+++ gen/org/ruleant/ariadne/R.java
@@ -14,9 +14,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070002;
-        public static final int textView1=0x7f070000;
-        public static final int textView2=0x7f070001;
+        public static final int button_getLocationProvider=0x7f070000;
+        public static final int menu_settings=0x7f070003;
+        public static final int textView_Location=0x7f070001;
+        public static final int textView_LocationProvider=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -26,9 +27,11 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int first_message=0x7f040002;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040003;
+        public static final int button_getLocationProvider=0x7f040004;
+        public static final int location=0x7f040003;
+        public static final int location_provider=0x7f040002;
+        public static final int menu_settings=0x7f040005;
+        public static final int none=0x7f040001;
     }
     public static final class style {
         /** 
--- src/org/ruleant/ariadne/MainActivity.java
+++ src/org/ruleant/ariadne/MainActivity.java
@@ -1,15 +1,56 @@
+/**
+ * Main Activity
+ * 
+ * Copyright (C) 2012 Dieter Adriaenssens
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @author Dieter Adriaenssens <ruleant@users.sourceforge.net>
+ */
 package org.ruleant.ariadne;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
+/**
+ * Main Activity class
+ * 
+ * @author Dieter Adriaenssens <ruleant@users.sourceforge.net>
+ */
 public class MainActivity extends Activity {
-
+	private LocationManager locationManager;
+	private String providerName = "";
+	private String location = "";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		locationManager =
+				(LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		
+		if (! getLocationProvider().isEmpty()) {
+			refreshInterface();
+		}
+
 	}
 
 	@Override
@@ -19,4 +60,63 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	/**
+	 * Called when the user clicks the Renew provider button
+	 * 
+	 * @return void
+	 */
+	public void renewProvider(View view) {
+		getLocationProvider();
+		refreshInterface();
+	}
+	
+	/**
+	 * Retrieve Location Provider
+	 * 
+	 * Define best location provider based on certain criteria
+	 * 
+	 * @return String
+	 */
+	private String getLocationProvider() {
+		// Retrieve a list of location providers that have fine accuracy, no monetary cost, etc
+		Criteria criteria = new Criteria();
+		criteria.setAccuracy(Criteria.ACCURACY_FINE);
+		criteria.setCostAllowed(false);
+		criteria.setPowerRequirement(Criteria.POWER_LOW);
+		
+		if (locationManager != null) {
+			providerName = locationManager.getBestProvider(criteria, true);
+		}
+		
+		return providerName;
+	}
+	
+	/**
+	 * refresh interface messages
+	 * 
+	 * Refresh the values of Location Provider, Location, ...
+	 * 
+	 * @return void
+	 */
+	private void refreshInterface() {
+		// Refresh locationProvider
+		TextView tv_provider = (TextView) findViewById(R.id.textView_LocationProvider);
+		String providerText = getResources().getString(R.string.location_provider) + ": ";
+		if (providerName.isEmpty()) {
+			providerText += getResources().getString(R.string.none);
+		} else {
+			providerText += providerName;
+		}
+		tv_provider.setText(providerText);
+		
+		// Refresh Location
+		TextView tv_location = (TextView) findViewById(R.id.textView_Location);
+		String locationText = getResources().getString(R.string.location) + ": ";
+		if (location.isEmpty()) {
+			locationText += getResources().getString(R.string.none);
+		} else {
+			locationText += location;
+		}
+		tv_location.setText(locationText);
+	}
 }
