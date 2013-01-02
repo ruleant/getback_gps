84e5c08e5870f71aa4be352367a3371a76077ce9
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2013-01-02 20:08:12 +0100

    create LocationStore class

---
+++ src/org/ruleant/ariadne/LocationStore.java
@@ -0,0 +1,41 @@
+/**
+ * Location Store
+ * 
+ * Copyright (C) 2012 Dieter Adriaenssens
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @package org.ruleant.ariadne
+ * @author  Dieter Adriaenssens <ruleant@users.sourceforge.net>
+ */
+package org.ruleant.ariadne;
+
+/**
+ * Location Store saves a location
+ * 
+ * This object will store a location for future use, and will the save the location
+ * when the application is stopped
+ * 
+ * @package org.ruleant.ariadne
+ * @author  Dieter Adriaenssens <ruleant@users.sourceforge.net>
+ */
+public class LocationStore {
+	
+	/**
+	 * Constructor
+	 */
+	public LocationStore() {
+		
+	}
+}
