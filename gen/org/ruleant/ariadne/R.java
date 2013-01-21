725b3ebe991cc399af0aaf0a84d95763fa89573a
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2013-01-21 20:27:29 +0100

    add Settings Screen (with examples)

--- gen/org/ruleant/ariadne/R.java
+++ gen/org/ruleant/ariadne/R.java
@@ -8,6 +8,12 @@
 package org.ruleant.ariadne;
 
 public final class R {
+    public static final class array {
+        public static final int pref_example_list_titles=0x7f060000;
+        public static final int pref_example_list_values=0x7f060001;
+        public static final int pref_sync_frequency_titles=0x7f060002;
+        public static final int pref_sync_frequency_values=0x7f060003;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -15,56 +21,78 @@ public final class R {
         public static final int ic_menu_refresh=0x7f020001;
     }
     public static final class id {
-        public static final int button_getLocation=0x7f070008;
-        public static final int button_getLocationProvider=0x7f070004;
-        public static final int menu_about=0x7f07000d;
-        public static final int menu_refresh=0x7f07000c;
-        public static final int menu_settings=0x7f07000a;
-        public static final int menu_storelocation=0x7f07000b;
-        public static final int textView_Location=0x7f070005;
-        public static final int textView_LocationProvider=0x7f070006;
-        public static final int textView_StoredLocation=0x7f070007;
-        public static final int textView_ToDestination=0x7f070009;
-        public static final int textview_copyright=0x7f070002;
-        public static final int textview_license=0x7f070003;
-        public static final int textview_updated=0x7f070001;
-        public static final int textview_version=0x7f070000;
+        public static final int button_getLocation=0x7f090008;
+        public static final int button_getLocationProvider=0x7f090004;
+        public static final int menu_about=0x7f09000d;
+        public static final int menu_refresh=0x7f09000c;
+        public static final int menu_settings=0x7f09000a;
+        public static final int menu_storelocation=0x7f09000b;
+        public static final int textView_Location=0x7f090005;
+        public static final int textView_LocationProvider=0x7f090006;
+        public static final int textView_StoredLocation=0x7f090007;
+        public static final int textView_ToDestination=0x7f090009;
+        public static final int textview_copyright=0x7f090002;
+        public static final int textview_license=0x7f090003;
+        public static final int textview_updated=0x7f090001;
+        public static final int textview_version=0x7f090000;
     }
     public static final class layout {
         public static final int activity_about=0x7f030000;
         public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f080000;
     }
     public static final class string {
-        public static final int about=0x7f040018;
-        public static final int accuracy=0x7f04000d;
-        public static final int altitude=0x7f04000a;
-        public static final int app_name=0x7f040000;
-        public static final int bearing=0x7f04000c;
-        public static final int button_getLocation=0x7f040014;
-        public static final int button_getLocationProvider=0x7f040013;
-        public static final int copyright=0x7f040001;
-        public static final int distance=0x7f040010;
-        public static final int latitude=0x7f040008;
-        public static final int license=0x7f040019;
-        public static final int location=0x7f040005;
-        public static final int location_provider=0x7f040004;
-        public static final int longitude=0x7f040009;
-        public static final int menu_settings=0x7f04001a;
-        public static final int none=0x7f040002;
-        public static final int provider_no_support=0x7f040015;
-        public static final int raw=0x7f040011;
-        public static final int refresh=0x7f040012;
-        public static final int speed=0x7f04000b;
-        public static final int store_location=0x7f040007;
-        public static final int stored_location=0x7f040006;
-        public static final int timestamp=0x7f04000e;
-        public static final int to_dest=0x7f04000f;
-        public static final int unknown=0x7f040003;
-        public static final int updated=0x7f040017;
-        public static final int version=0x7f040016;
+        public static final int about=0x7f050018;
+        public static final int accuracy=0x7f05000d;
+        public static final int altitude=0x7f05000a;
+        public static final int app_name=0x7f050000;
+        public static final int bearing=0x7f05000c;
+        public static final int button_getLocation=0x7f050014;
+        public static final int button_getLocationProvider=0x7f050013;
+        public static final int copyright=0x7f050001;
+        public static final int distance=0x7f050010;
+        public static final int latitude=0x7f050008;
+        public static final int license=0x7f050019;
+        public static final int location=0x7f050005;
+        public static final int location_provider=0x7f050004;
+        public static final int longitude=0x7f050009;
+        public static final int menu_settings=0x7f05001a;
+        public static final int none=0x7f050002;
+        public static final int pref_default_display_name=0x7f050020;
+        public static final int pref_description_social_recommendations=0x7f05001e;
+        /**  Example settings for Data & Sync 
+         */
+        public static final int pref_header_data_sync=0x7f050022;
+        /**  Strings related to Settings 
+ Example General settings 
+         */
+        public static final int pref_header_general=0x7f05001c;
+        /**  Example settings for Notifications 
+         */
+        public static final int pref_header_notifications=0x7f050025;
+        public static final int pref_ringtone_silent=0x7f050028;
+        public static final int pref_title_add_friends_to_messages=0x7f050021;
+        public static final int pref_title_display_name=0x7f05001f;
+        public static final int pref_title_new_message_notifications=0x7f050026;
+        public static final int pref_title_ringtone=0x7f050027;
+        public static final int pref_title_social_recommendations=0x7f05001d;
+        public static final int pref_title_sync_frequency=0x7f050023;
+        public static final int pref_title_system_sync_settings=0x7f050024;
+        public static final int pref_title_vibrate=0x7f050029;
+        public static final int provider_no_support=0x7f050015;
+        public static final int raw=0x7f050011;
+        public static final int refresh=0x7f050012;
+        public static final int speed=0x7f05000b;
+        public static final int store_location=0x7f050007;
+        public static final int stored_location=0x7f050006;
+        public static final int timestamp=0x7f05000e;
+        public static final int title_activity_settings=0x7f05001b;
+        public static final int to_dest=0x7f05000f;
+        public static final int unknown=0x7f050003;
+        public static final int updated=0x7f050017;
+        public static final int version=0x7f050016;
     }
     public static final class style {
         /** 
@@ -88,10 +116,16 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f070001;
+    }
+    public static final class xml {
+        public static final int pref_data_sync=0x7f040000;
+        public static final int pref_general=0x7f040001;
+        public static final int pref_headers=0x7f040002;
+        public static final int pref_notification=0x7f040003;
     }
 }
