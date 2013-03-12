e1b7e55dc195cdbc15585ec98e491b33aa17ef3d
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2013-03-12 19:46:15 +0100

    rename class, update disclaimer mentioning origin

--- src/org/ruleant/ariadne/FormatUtils.java
+++ src/org/ruleant/ariadne/FormatUtils.java
@@ -1,26 +1,33 @@
-/*
- * Copyright (C) 2010- Peer internet solutions
- * 
- * This file is part of mixare.
- * 
+/**
+ * Class to format distances, speeds and numbers.
+ *
+ * Copyright (C) 2010 Peer internet solutions
+ * Copyright (C) 2013 Dieter Adriaenssens
+ *
+ * This file is based on a class that is part of mixare.
+ * The original source can be found on :
+ * http://www.java2s.com/Code/Android/Date-Type/FormatDistance.htm
+ * See commit logs for changes.
+ *
  * This program is free software: you can redistribute it and/or modify it 
  * under the terms of the GNU General Public License as published by 
  * the Free Software Foundation, either version 3 of the License, or 
  * (at your option) any later version. 
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License 
  * for more details. 
- * 
+ *
  * You should have received a copy of the GNU General Public License along with 
  * this program. If not, see <http://www.gnu.org/licenses/>
+ *
+ * @package org.ruleant.ariadne
+ * @author  Dieter Adriaenssens <ruleant@users.sourceforge.net>
  */
-//package org.icare;
+package org.ruleant.ariadne;
 
-class MixUtils {
-
-
+class FormatUtils {
   public static String formatDist(float meters) {
     if (meters < 1000) {
       return ((int) meters) + "m";
