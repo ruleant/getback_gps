87f0bbd70393e0eaeb1ca6e4ecb7063af700d8d7
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2013-04-06 22:58:18 +0200

    fix JavaDoc

--- src/org/ruleant/ariadne/SettingsActivity.java
+++ src/org/ruleant/ariadne/SettingsActivity.java
@@ -15,7 +15,7 @@ import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 
 /**
- * A {@link PreferenceActivity} that presents a set of application settings. On
+ * A PreferenceActivity that presents a set of application settings. On
  * handset devices, settings are presented as a single list. On tablets,
  * settings are split by category, with category headers shown to the left of
  * the list of settings.
