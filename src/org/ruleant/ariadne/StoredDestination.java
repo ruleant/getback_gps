5bce041e4955e8e88157457100d8759d2fa647dd
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2013-04-27 17:22:32 +0200

    create StoredDestination class

---
+++ src/org/ruleant/ariadne/StoredDestination.java
@@ -0,0 +1,66 @@
+package org.ruleant.ariadne;
+/**
+ * Stored Destination
+ *
+ * Copyright (C) 2012-2013 Dieter Adriaenssens
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package org.ruleant.ariadne
+ * @author  Dieter Adriaenssens <ruleant@users.sourceforge.net>
+ */
+
+
+import java.util.Locale;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.location.Location;
+
+/**
+ * StoredDestination saves a location, it will store a location for future use,
+ * and will the save the location when the application is stopped.
+ *
+ * @author Dieter Adriaenssens <ruleant@users.sourceforge.net>
+ */
+public class StoredDestination extends StoredLocation {
+    /**
+     * SharedPreferences location for StoredDestination class.
+     */
+    public static final String DEFAULT_PREF_NAME = "stored_destination";
+
+    /**
+     * Constructor.
+     *
+     * @param context        Context of the Android app
+     * @param sharedPrefName Name of Shared Preferences file name
+     */
+    public StoredDestination(final Context context, final String sharedPrefName) {
+        super(context, sharedPrefName);
+    }
+
+    /**
+     * Set Location.
+     *
+     * @param location New location
+     */
+    public void setLocation(final Location location) {
+        Location tempLocation = new Location("");
+        
+        tempLocation.setLongitude(location.getLongitude());
+        tempLocation.setLatitude(location.getLatitude());
+        
+        super.setLocation(tempLocation);
+    }
+}
