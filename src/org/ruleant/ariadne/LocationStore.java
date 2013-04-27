5674c8115c5b31cc5c1188d0ed8312917f652620
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2013-04-27 16:14:09 +0200

    update comment, always store shared preferences under US English locale, to avoid data loss when default locale of the device is changed.

--- src/org/ruleant/ariadne/LocationStore.java
+++ src/org/ruleant/ariadne/LocationStore.java
@@ -126,12 +126,9 @@ public class LocationStore {
             return;
         }
 
-        /* set Locale temporary to US to avoid Android bug 5734
-            https://code.google.com/p/android/issues/detail?id=5734
-            Location.convert(String) is localization independent,
-            so it throws an exception when a parameter contains a ","
-            instead of a "." as decimal separator
-            changing the local to US, converts the double to a string with a "."
+        /* Set Locale temporarily to US English to make sure that the data
+         * is always stored with the same locale, to avoid data loss
+         * when the default locale of the device is changed.
          */
         Locale originalLocale = Locale.getDefault();
         Locale.setDefault(Locale.US);
@@ -171,7 +168,7 @@ public class LocationStore {
         // Commit the edits!
         editor.commit();
 
-        // set default locale back to original, workaround for Android bug 5734
+        // set default locale back to original
         Locale.setDefault(originalLocale);
     }
 
