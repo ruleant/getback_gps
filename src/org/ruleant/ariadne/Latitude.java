7d31f8f62e503e7ecc7f05065830bb4db3d356b7
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2013-07-21 20:42:03 +0200

    add Latitude class

---
+++ src/org/ruleant/ariadne/Latitude.java
@@ -0,0 +1,30 @@
+/**
+ * Class for formatting latitude.
+ *
+ * Copyright (C) 2012-2013 Dieter Adriaenssens
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package org.ruleant.ariadne
+ * @author  Dieter Adriaenssens <ruleant@users.sourceforge.net>
+ */
+package org.ruleant.ariadne;
+
+/**
+ * Class for formatting latitude.
+ *
+ * @author  Dieter Adriaenssens <ruleant@users.sourceforge.net>
+ */
+public class Latitude extends AbstractGeoCoordinate{
+}
