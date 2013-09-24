c358ecb8192c13145784d2734cc315994dc20668
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2013-09-24 21:13:48 +0200

    create NavigationView

---
+++ src/org/ruleant/ariadne/NavigationView.java
@@ -0,0 +1,61 @@
+/**
+ * Navigation View
+ *
+ * Copyright (C) 2012-2013 Dieter Adriaenssens
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package org.ruleant.ariadne
+ * @author Dieter Adriaenssens <ruleant@users.sourceforge.net>
+ */
+package org.ruleant.ariadne;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Navigation view is used to indicate the direction to the destination.
+ *
+ * @author Dieter Adriaenssens <ruleant@users.sourceforge.net>
+ */
+public class NavigationView extends View {
+    /**
+     * Paint used for drawing
+     */
+    Paint mPaint = new Paint();
+
+    /**
+     * Constructor.
+     *
+     * @param context App context
+     */
+    public NavigationView(Context context) {
+        super(context);
+        mPaint.setColor(Color.BLACK);
+    }
+
+    public NavigationView(Context context, AttributeSet attributes) {
+        super(context, attributes);
+        mPaint.setColor(Color.BLACK);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        canvas.drawLine(50, 50, 50, 10, mPaint);
+    }
+}
