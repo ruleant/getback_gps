861b1a6bb53fe771f2fc75ca0da455ce6a512313
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2013-11-16 12:27:11 +0100

    add tests for navigator.isDestinationReached()

--- test/NavigatorTest.java
+++ test/NavigatorTest.java
@@ -185,6 +185,7 @@ public class NavigatorTest extends TestCase {
 
         assertFalse(navigator.isLocationAccurate());
         assertFalse(navigator.isBearingAccurate());
+        assertFalse(navigator.isDestinationReached());
     }
 
     /**
@@ -341,6 +342,42 @@ public class NavigatorTest extends TestCase {
         assertEquals(
                 FormatUtils.normalizeAngle(DIR_LOC1_3 - DIR_LOC2_1),
                 navigator.getRelativeDirection());
+    }
+
+    /**
+     * Tests isDestinationReached.
+     */
+    public final void testIsDestinationReached() {
+        // set location
+        navigator.setLocation(loc1);
+
+        // location is not accurate, check should fail
+        assertFalse(navigator.isDestinationReached());
+
+        // location is accurate, but no destination is set
+        when(loc1.isRecent()).thenReturn(true);
+        when(loc1.getAccuracy()).thenReturn(ACCURACY_OK_40);
+        assertFalse(navigator.isDestinationReached());
+
+        // set destination, same location as location
+        navigator.setDestination(loc1);
+
+        // destination and current location are the same,
+        // check should return true
+        assertEquals((float) 0, navigator.getDistance());
+        assertTrue(navigator.isDestinationReached());
+
+        // set new destination
+        navigator.setDestination(loc2);
+
+        // distance between location and destination is smaller than accuracy,
+        // check should return true
+        assertTrue(navigator.isDestinationReached());
+
+        // distance between location and destination is bigger than accuracy,
+        // check should return false
+        when(loc1.getAccuracy()).thenReturn(ACCURACY_OK_10);
+        assertFalse(navigator.isDestinationReached());
     }
 
     /**
