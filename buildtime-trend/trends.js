3e1b25755c926186c5e8a4e2912e2b28014bdcc6
travis-ci <travis@travis-ci.org>
2014-10-09 20:11:47 +0000

    buildtime-trend of build 504.1 synchronised with gh-pages

--- buildtime-trend/trends.js
+++ buildtime-trend/trends.js
@@ -1,6 +1,6 @@
 /* vim: set expandtab sw=4 ts=4: */
 /**
- * Gathers build related data.
+ * Analyse and visualise trend data using the Keen.io API.
  *
  * Copyright (C) 2014 Dieter Adriaenssens <ruleant@users.sourceforge.net>
  *
@@ -489,8 +489,14 @@ function htmlEntities(str) {
 
 /**
  * Merge data from several series, with identical X-axis labels
+ * 
+ * Parameters :
+ * - data : Keen.io query results
+ * - index_captions : Captions for the index of the values
+ * - value_fieldname : name of the value field in the query result array
+ * - series_captions : captions for the data series
  */
-function mergeSeries(data, index_captions, value_caption, series_captions) {
+function mergeSeries(data, index_captions, value_fieldname, series_captions) {
     chart_data = [];
     // create and populate data array
     for (i = 0; i < index_captions.length; i++) {
@@ -506,7 +512,7 @@ function mergeSeries(data, index_captions, value_caption, series_captions) {
         timeframe_caption = series_captions[j];
         // copy query data into the populated array
         for (i = 0; i < timeframe_result.length; i++) {
-            index = parseInt(timeframe_result[i][value_caption])
+            index = parseInt(timeframe_result[i][value_fieldname])
             chart_data[index][timeframe_caption] = timeframe_result[i]["result"];
         }
     }
