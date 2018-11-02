dda7865ea8586df5d6428225bc51e7cfb5630933
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2018-11-02 17:41:09 +0100

    build against android 8 (API 26)

--- .utility/deploy-sdk-to-m2-repo.sh
+++ .utility/deploy-sdk-to-m2-repo.sh
@@ -20,7 +20,7 @@
 # deploy android SDK jar to local maven repository
 if [[ -d maven-android-sdk-deployer/.git ]]; then cd maven-android-sdk-deployer; git pull; else git clone https://github.com/mosabua/maven-android-sdk-deployer.git; cd maven-android-sdk-deployer; fi
 # only deploy SDK 6 (API 23)
-mvn clean install -pl platforms/android-23
+mvn clean install -pl platforms/android-26
 
 # return to initial path
 cd ..
