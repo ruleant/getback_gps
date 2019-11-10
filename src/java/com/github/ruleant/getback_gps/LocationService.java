5d9e92ee565a3382c86fedfdbbacb0157f1cef20
constambeys <timo0190@gmail.com>
2019-11-10 11:09:42 +0200

    add copyright notice and some code remarks

--- src/java/com/github/ruleant/getback_gps/LocationService.java
+++ src/java/com/github/ruleant/getback_gps/LocationService.java
@@ -2,6 +2,7 @@
  * Location Service
  *
  * Copyright (C) 2012-2019 Dieter Adriaenssens
+ * Copyright (C) 2019 Timotheos Constambeys
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
